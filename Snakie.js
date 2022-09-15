f715e94930281f7c5066b36ec603e41e58281858
k1rlm <104268743+k1rlm@users.noreply.github.com>
2022-09-15 18:25:11 +0300

    Update Snakie.js

--- Snakie.js
+++ Snakie.js
@@ -1,11 +1,8 @@
-// Я крч это, ну, кхм как там его, в общем, да пошли вы нахуй
-// Эйей мне так хочется любыи с кем то ебаться эйей может это будешь ты я главный гангстер на районе 
-// npm --build new access c:/users/k1rlm/desktop/snakie/snkie.js
 class Snake {                  
     constructor(x, y, size) {
         this.x = x
         this.y = y
-        this.size = size                          // Начинаем анальную еблю с создания двух классов
+        this.size = size                          // Using class with constructor
         this.tail = [{x:this.x, y:this.y}]
         this.rotateX = 0
         this.rotateY = 1
@@ -24,7 +21,7 @@ class Snake {
                 x: this.tail[this.tail.length - 1].x - this.size,
                 y: this.tail[this.tail.length - 1].y
             }
-        } else if (this.rotateY == 1) {                                  // Марина Валентиновна я не знаю ось координат
+        } else if (this.rotateY == 1) {                                  // Snake's tale length
             newRect = {
                 x: this.tail[this.tail.length - 1].x,
                 y: this.tail[this.tail.length - 1].y + this.size
@@ -69,14 +66,14 @@ class Apple{
 
 
 const canvas = document.getElementById("canvas");
-const canvasContext = canvas.getContext('2d');                    // Приклеиваем на соплях наш html юзая камаз
+const canvasContext = canvas.getContext('2d');                    // Including our html file
 
 window.onload = () => {
     gameLoop()
 }
 
 function gameLoop() {
-    setInterval(show, 1000/20)                                     // Делаем графику как в киберпунке
+    setInterval(show, 1000/20)                                     // Graphics
 }
 
 function show() {
@@ -85,7 +82,7 @@ function show() {
 }
 
 function update() {
-    canvasContext.clearRect(0, 0, canvas.width, canvas.height);  // Розы живут в навозе пацаны в поносе в мы мешали любовь с табаком
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);  // Updating our game functions
     snake.move()
     eatApple()
     checkHitWall()
@@ -93,7 +90,7 @@ function update() {
 
 function eatApple() {
     if(snake.tail[snake.tail.length - 1].x == apple.x &&
-        snake.tail[snake.tail.length - 1].y == apple.y){              // Кормим змейку агушей
+        snake.tail[snake.tail.length - 1].y == apple.y){              // Making actions when snake eating apple
             snake.tail[snake.tail.length] = {x:apple.x, y: apple.y}
             apple = new Apple();
         }
@@ -104,7 +101,7 @@ function checkHitWall() {
 
     if (headTail.x == - snake.size) {
         headTail.x = canvas.width - snake.size
-    } else if (headTail.x == canvas.widh) {              // Чекхитвол а хуй его знает че за параша
+    } else if (headTail.x == canvas.widh) {              // Cheking snake if it reached the wall
         
         headTail.x = 0
     } else if (headTail.y == - snake.size) {
@@ -115,11 +112,11 @@ function checkHitWall() {
 }
 
 function draw() {
-    createRect(0,0,canvas.width, canvas.height, "black");  // Делаем эпичный фон из онимэ
+    createRect(0,0,canvas.width, canvas.height, "black");  // Background
     createRect(0,0, canvas.width, canvas.height);
 
     for (let i = 0; i < snake.tail.length; i++){
-        createRect(snake.tail[i].x + 2.5, snake.tail[i].y + 2.5, // Растим хуй с помощью столовой соды
+        createRect(snake.tail[i].x + 2.5, snake.tail[i].y + 2.5, 
             snake.size - 5, snake.size- 5, "white");
     }
 
@@ -130,7 +127,7 @@ function draw() {
 }
 
 function createRect(x,y,width, height,color) {
-    canvasContext.fillStyle = color                // Артем скачай доту
+    canvasContext.fillStyle = color                
     canvasContext.fillRect(x, y, width, height);
 }
 
@@ -141,7 +138,7 @@ window.addEventListener("keydown", (event) => {
             snake.rotateY = 0
         } else if (event.keyCode == 38 && snake.rotateY != 1) {
             snake.rotateX = 0
-            snake.rotateY = -1                                        // И самая главная хуйня с которой СУКА Я ЕБАЛСЯ ВЕСЬ ДЕНЬ
+            snake.rotateY = -1                                        // Setting up the actions of keyboard buttons
         } else if (event.keyCode == 39 && snake.rotateX != -1) {
             snake.rotateX = 1
             snake.rotateY = 0
@@ -154,13 +151,3 @@ window.addEventListener("keydown", (event) => {
 
 const snake = new Snake(20,20,20); 
 let apple = new Apple();
-// Эпик финал
-// лпагмлрлдроагелеовопшпгашешнщпгашрщршрзнщнзозрщрщрщрщпнзолалмдмомщтш
-//лмовоаоаоаорщрлпшощпшрщалааоыгцлалрдишмшищрдидпщрщрдпдрлп
-// угалалпвгплаонзроарлилмлпшпшапшапдрулашпшвнпопосоашпшплплгпо
-//оагагалршкшешишсоанынаомомоаомшмосгмгмоаомшпгпгаалмлмлмомоалгм
-//лппоагцнагпгпугршпналрдрдиоалррдплалаоагпшрлргидршпшплмбеагаш
-//лпнашпшегаагвагацнамлрщлхлзршзорлпшрщршршпшппдтшпашагаашаплишшршр
-//гг вагагпганунугпдпгаоиоснагагагаганагкагаганаопшмшм
-// эаэпэааээаэаэаэаэаэааааааа не пизди мой код я сам его спиздил у ютубера индуса с 200 подписчиками 
-// фортнайтеры и геншин фаги сасать
